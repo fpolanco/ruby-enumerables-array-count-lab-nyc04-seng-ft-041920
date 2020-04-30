5f0e5975a34bb385fd4799bcd4dd3319fc7372b4
Franchell Polanco <polanco.franchell@gmail.com>
2020-04-30 17:26:51 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -13,7 +13,7 @@ def count_empty_strings(array)
   empty_string = []
   array.count do |item|
     if item.to_s.empty?
-      binding.pry
+    
       empty_string << item
     end
      # Return the total number of EMPTY strings in the provided array using the count enumerable
